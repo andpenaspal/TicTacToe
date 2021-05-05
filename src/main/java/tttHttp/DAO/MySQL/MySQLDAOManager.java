b1586330b1b0be58b1521cdf900915fad9b68acf
Andres Penas Palmeiro <andpenaspal@gmail.com>
2021-05-05 11:47:26 +0100

    Log for MySQLDAOManager

--- src/main/java/tttHttp/DAO/MySQL/MySQLDAOManager.java
+++ src/main/java/tttHttp/DAO/MySQL/MySQLDAOManager.java
@@ -1,9 +1,12 @@
 package tttHttp.DAO.MySQL;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import tttHttp.DAO.DAOManager;
 import tttHttp.DAO.GameDAO;
 import tttHttp.DAO.PlayerDAO;
 import tttHttp.DAO.exceptions.DAOException;
+import tttHttp.controllers.PlayerController;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,6 +18,7 @@ public class MySQLDAOManager implements DAOManager {
     private GameDAO games = null;
 
     private Connection connection;
+    private final Logger LOG = LoggerFactory.getLogger(MySQLDAOManager.class);
 
     public MySQLDAOManager(String host, int port, String database, String username, String password) throws DAOException,
             ClassNotFoundException {
@@ -22,6 +26,7 @@ public class MySQLDAOManager implements DAOManager {
         try {
             connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, username, password);
         } catch (SQLException throwables) {
+            LOG.error("Error trying to connect to the DDBB", throwables);
             throw new DAOException("Problem trying to connect to the DDBB", throwables);
         }
     }
@@ -46,7 +51,7 @@ public class MySQLDAOManager implements DAOManager {
         try {
             connection.close();
         } catch (SQLException throwables) {
-            //TODO: Log
+            LOG.error("Error trying to close the Connection to the DDBB", throwables);
             throw new DAOException("Problem trying to close the connection", throwables);
         }
     }
